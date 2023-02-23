07fc513c0b2238ff1c15fd0488dc96cadbfae7ab
Yhonathan Pavan <yhonathannpavan@gmail.com>
2023-02-22 22:52:55 -0300

    Base do projeto

---
+++ src/main/java/com/tcc/doapet/DoaPetApplication.java
@@ -0,0 +1,13 @@
+package com.tcc.doapet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoaPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoaPetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tcc/doapet/DoaPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcc.doapet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoaPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
